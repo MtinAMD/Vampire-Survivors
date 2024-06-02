f903558f19b25123df890dea2f2a02f9a2de928c
Aair05 <152048407+Aair05@users.noreply.github.com>
2024-06-02 16:11:01 -0700

    Blood
    adding a blood animation which will be shown after enemies death. Besides this, for good locking in the map, we can also see the footprint of dead.

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Mathematics;
 using UnityEngine;
 using UnityEngine.TextCore.Text;
 
@@ -48,12 +49,15 @@ public class Enemy : MonoBehaviour
         targetCharacter.TakeDamage(damage);
     }
 
+    /***********************************/
+    public GameObject Blood;
     public void TakeDammage(int dammage)
     {
         hp -= dammage;
         if (hp < 1)
         {
             Destroy(gameObject);
+            Instantiate(Blood, transform.position, quaternion.identity);
         }
     }
 }

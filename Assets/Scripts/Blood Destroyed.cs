28c4ca74e4e08e3296b3cd85c809e0ab2cef82c2
Aair05 <152048407+Aair05@users.noreply.github.com>
2024-06-03 13:42:39 -0700

    Destroy extra clone
    In this edition I destroy extra enemies blood animation which slow down our run speed during playing the game.

---
+++ Assets/Scripts/Blood Destroyed.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Blood : MonoBehaviour
+{
+    void Update()
+    {
+        Destroy(gameObject, 3f);
+    }
+}
